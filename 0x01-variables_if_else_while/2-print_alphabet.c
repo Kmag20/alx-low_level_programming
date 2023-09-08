6735f884d707645129313c3a4dca5d64a2493ba2
Kmag20 <owenmawira@gmail.com>
2023-09-08 13:57:46 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of our code
+ *
+ * Return: Zero on success
+ */
+int main(void)
+{
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
