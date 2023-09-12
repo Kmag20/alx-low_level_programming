a2f00ea99e4ff70185fd55591c22e26e2b264d01
Kmag20 <owenmawira@gmail.com>
2023-09-12 13:34:34 -0700

    C

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,6 +4,7 @@
  * _isalpha - returns 1 if the input is an alphabet
  * (lowercase or alphabet), it also returns 0 if otherwise
  *
+ * @c: character type 
  * Return: 1 for lowercase , 0 if otherwise
  */
 int _isalpha(int c)
