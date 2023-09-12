8d3711cb87d64d3df5324ef1feb96c79bd9c1044
Kmag20 <owenmawira@gmail.com>
2023-09-12 13:36:39 -0700

    C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,7 @@
 /**
  * print_sign - Determines 1 if n is greater, equal or less than 0
  *
+ * @n: integer data type
  * Return: 1 if n is greater than 0. 0 is zero. -1 is less than 0
  */
 int print_sign(int n)
