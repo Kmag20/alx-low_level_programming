42a3d25fed20c6c97367d08bbeb08ae673ed5ddf
Kmag20 <owenmawira@gmail.com>
2023-09-14 14:29:24 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * main - test the function which prints whether input is +ve or -ve
+ *
+ * Return: 0 on Success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
