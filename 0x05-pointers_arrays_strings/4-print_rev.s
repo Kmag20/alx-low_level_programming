9175e6cb11a66208025e74284d4a6ccd637ce772
Kmag20 <owenmawira@gmail.com>
2023-09-18 14:39:59 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.s
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _print_rev - prints a strign in reverse
+  * @s: String 
+  *
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int i, j, len = _strlen(s);
+	char temp;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		str[i] = str[len - i -1];
+		str[len - i -1] = temp;
+	}
+
+	for (j = 0; j < len - 1; j++)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+
+
+}
