ac3261a699cf144c5190df7f2711d95f4e7b47a1
Kmag20 <owenmawira@gmail.com>
2023-09-18 15:00:51 -0700

    Print rev string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,17 +9,15 @@
 int _strlen(char *s);
 void print_rev(char *s)
 {
-	int i, j, len = _strlen(s);
-	char temp;
+	int numchar;
 
-	for (i = 0; i < len / 2; i++)
-	{
-		temp = s[i];
-		s[i] = s[len - i -1];
-		s[len - i -1] = temp;
-	}
+        for (numchar = 0; *s != '\0'; s++)
+        {
+                numchar++;
+        }
+	int i, j, len = numchar;
 
-	for (j = 0; j < len - 1; j++)
+	for (j = len; j >= 1; j--)
 	{
 		_putchar(s[j]);
 	}
