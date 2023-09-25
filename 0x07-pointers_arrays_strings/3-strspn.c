2f93506f8a636fdf5dd49076f17f85ee814aeeb9
Kmag20 <owenmawira@gmail.com>
2023-09-25 15:26:36 -0700

    Mandatory

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,32 @@
 #include "mainn.h"
 
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: initial segment.
- * @accept: accepted bytes.
- * Return: the number of accepted bytes.
- */
+  * _strspn - search a string for a set of bytes
+  * @s: source string
+  * @accept: accepted string
+  *
+  * Return: number of bytes in the init segment
+  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, bool;
+	unsigned int a = 0, b, t = 0;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	while (accept[a])
 	{
-		bool = 1;
-		for (j = 0; *(accept + j) != '\0'; j++)
+		b = 0;
+
+		while (s[b] != 32)
 		{
-			if (*(s + i) == *(accept + j))
+			if (accept[a] == s[b])
 			{
-				bool = 0;
-				break;
+				t++;
 			}
+
+			b++;
 		}
-		if (bool == 1)
-			break;
+
+		a++;
 	}
-	return (i);
+
+	return (t);
 }
