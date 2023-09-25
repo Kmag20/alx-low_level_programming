e4a0604463050e0b6d09801b69e0d8bace7eb94f
Kmag20 <owenmawira@gmail.com>
2023-09-25 15:10:46 -0700

    Mandatory

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - pointer to the first occurrence of char c in string s
+ *
+ * @s:string
+ * @c: character 
+ *
+ * Return: pointer to s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (c == s[i]) return s[i];
+	}
+	return ('\0');
+}
