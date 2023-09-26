c8272e71969e9baadb60c9bbd9963a644ec95342
Kmag20 <owenmawira@gmail.com>
2023-09-26 10:49:52 -0700

    _strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  * _strspn - search a string for a set of bytes
+  * @s: source string
+  * @accept: accepted string
+  *
+  * Return: number of bytes in the init segment
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+        unsigned int i, j;
+        unsigned int cnt = 0;
+        unsigned int found_match;
+
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                found_match = 0;
+                for (j = 0; j < accept[j] != '\0'; j++)
+                {
+                        if (accept[j] == s[i])
+                                found_match = 1;
+                                break;
+                }
+                if (!(found_match) break;
+                cnt++;
+        }
+
+        return (cnt);
+}
+
