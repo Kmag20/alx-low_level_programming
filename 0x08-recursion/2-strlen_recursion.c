b96396fa08f7295d9535e22cd4123cc22966474d
Kmag20 <owenmawira@gmail.com>
2023-09-27 13:15:14 -0700

    strlen recursively

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,13 +11,12 @@ int cnt = 0;
 
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		cnt++;
-		_strlen_recursion(s + 1);
+		return (0);
 	}
 	else
 	{
-		return (cnt);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
