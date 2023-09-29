4f16ad7d53b6e095968f63938c8ab4941c4cc13c
Kmag20 <owenmawira@gmail.com>
2023-09-29 02:45:54 -0700

    Multply args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		printf("Error");
 		return (1);
-	else
+	if (argc == 3)
 	{
 		result = atoi(argv[1] * atoi(argv[2]);
 		printf("%d\n", result);
