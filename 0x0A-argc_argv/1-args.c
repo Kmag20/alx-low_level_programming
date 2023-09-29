6334c9d757c4dc7892e4ce0e99873ba15d9a7769
Kmag20 <owenmawira@gmail.com>
2023-09-29 02:38:45 -0700

    Number of args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,12 +3,11 @@
 /**
  * main - prints the number of args passed
  *
- * @argc - argument count
- * @argv - argument vector
+ * @argc: argument count
+ * @argv: argument vector
  *
  * Return: 0 on success
  */
-
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
