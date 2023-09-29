0fd3edbbc686103bd07c0d032f22a380b37d8ac1
Kmag20 <owenmawira@gmail.com>
2023-09-29 02:50:24 -0700

    Multiply args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,10 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int result;
-
 	if (argc != 3)
-		printf("Error");
+		printf("Error\n");
 		return (1);
 
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
