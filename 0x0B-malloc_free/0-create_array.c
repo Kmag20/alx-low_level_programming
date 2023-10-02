bc24b4d55c6db8bcda205a8187e0d50799a9e1b1
Kmag20 <owenmawira@gmail.com>
2023-10-02 11:44:05 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ch = malloc(sizeof(char) * size);
-	if (ch == NULL) return (NULL);
-	if (size == 0) return ch;
+	if (ch == NULL) 
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		ch[i] = c;
