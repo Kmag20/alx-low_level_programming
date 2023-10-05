22040007bb98f475a26685bc34f937e1fa871ead
Kmag20 <owenmawira@gmail.com>
2023-10-05 11:30:24 -0700

    malloc checker

---
+++ 0x0C-more_malloc_free/0_malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - checks whether malloc works well 
+ *
+ * @b: bytes
+ *
+ * Return: pointer to alloced mem or 98 for erro
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+
+	return (s);
+}
