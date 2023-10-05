62edfa917e8727e9c58a8a46f98695964606d94f
Kmag20 <owenmawira@gmail.com>
2023-10-05 13:31:51 -0700

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(len * sizeof(int));
+	ptr = malloc(len * sizeof(*ptr));
 	if (ptr == NULL)
 		return (NULL);
 
