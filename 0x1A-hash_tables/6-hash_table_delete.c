c382b0d53c661dd93735610a98ab39032095fff9
Kmag20 <owenmawira@gmail.com>
2024-03-09 19:29:03 +0300

    Delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table
+ *
+ * @ht: Pointer to the hash table
+ * Return: None
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *next_node;
+
+	if (ht == NULL)
+		return;
+	
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			node = ht->array[i];
+			while (node)
+			{
+				next_node = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = next_node;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}	
