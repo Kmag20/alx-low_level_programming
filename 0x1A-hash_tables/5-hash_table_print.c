13c34f411c03e1e92969891fb05181815f0d1757
Kmag20 <owenmawira@gmail.com>
2024-03-09 18:29:11 +0300

    Print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,17 +2,6 @@
 #include <stdio.h>
 
 /**
- * struct KeyValuePair - Custom dictionary for easy printing
- *
- * @key: key
- * @value: value
- */
-/*typedef struct KeyValuePair {
-	char *key;
-	char *value;
-} dict;*/
-
-/**
  * hash_table_print - Prints a hash table
  *
  * @ht: Pointer to the hash table
@@ -22,16 +11,27 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *node;
+	int comma = 0;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[i];
-		while (node)
+		if (ht->array[i])
 		{
-			printf("'%s': '%s', ", node->key, node->value);
+			if (comma == 1)
+				printf(", ");
 
-			node = node->next;
+			node = ht->array[i];
+			while (node)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node)
+				{
+					printf(", ");
+				}
+			}
+			comma = 1;
 		}
 	}
 	printf("}\n");
